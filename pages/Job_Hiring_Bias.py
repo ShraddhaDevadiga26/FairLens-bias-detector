236b40ebb6c6e97e0bcfc76dffd05120434f24d0
ShraddhaDevadiga26 <shraddhadevadiga26@gmail.com>
2026-04-28 03:16:34 +0530

    Update Job_Hiring_Bias.py

--- pages/Job_Hiring_Bias.py
+++ pages/Job_Hiring_Bias.py
@@ -236,11 +236,11 @@ label, .stSelectbox label, .stTextArea label, .stTextInput label { color: #4a4a4
 """
 
 def get_client():
-    api_key = os.getenv("AIzaSyAbWBqsRwEhL_tLFO43mvfK8uEJFddM4mc")
+    api_key = os.getenv("AIzaSyAxlTW5Lj3iFaY2HNsjdaxuhuO4MqMQfoI")
     if not api_key:
         raise ValueError("GOOGLE_API_KEY environment variable not set")
     genai.configure(api_key=api_key)
-    return genai.GenerativeModel('gemini-1.5-flash')  
+    return genai.GenerativeModel('gemini-2.0-flash')  
 
 def extract_pdf_text(file):
     text = ""
